919223373149743676add44a4bcef625f904b609
AaronMillOro <aaron.ibt@gmail.com>
2019-02-10 03:48:05 +0100

    Add of REGEX block and example files of CodY regulon

--- PDF_scraper.py
+++ PDF_scraper.py
@@ -3,42 +3,38 @@
 
 from PyPDF2 import PdfFileReader
 import os
-#from os import listdir
-#from os.path import isfile, join
+import re
 
 mypath = os.getcwd()  #set directory containing PDFs
 print("PDF files in the directory -> ", mypath, "\n")
 
-count=0
-fullText="" #set as "empty" string
+count = 0
+fullText = "" #set as "empty" string
+genes = ""
 
 for pdfFile in os.listdir(mypath):
     if pdfFile.endswith(".pdf"):
         count+=1
         print("File ", pdfFile)
         textFileName = pdfFile.split(".")
-        textFileName = textFileName[0]+".csv" #set txt format
-# Open PDF files
+        textFileName = textFileName[0]+".csv" #set txt/csv format
+# Open PDF file
         openpdf = PdfFileReader(pdfFile, 'rb') # "rb" read binary
         nbPages = openpdf.numPages
-# Convert PDFs files into flat text
+# Convert PDF file into flat text
         for page in range(nbPages):
-            print("Page",page+1,"/", nbPages)
+            print("Reading page",page+1," of ", nbPages)
             pageObj = openpdf.getPage(page)
             text= pageObj.extractText()
             fullText = fullText + text
-        # print(fullText)
-
-# Export text as txt files
+        #print(fullText))
+# Use REGEX to get genes with the format: SAUSA300_XXXX
+# where XXXX means 4 digits
+        match = re.findall('^SAUSA300_\d{4}', fullText, re.MULTILINE) #re.MULTILINE scans over the long string
+        match = sorted(set(match)) #remove duplicates
+        match = '\n'.join(map(str, match)) # convert list into string
+        print("\n \n List of genes: \n \n",match)
+# Export text as txt/csv files
         output= open(textFileName ,"w+")
-        output.write(fullText)
+        output.write(match)
         output.close()
-
-
-
-#cmd /k for %%i in (*) do "c:\pdftotext\pdf2txt.py" -o c:\pdftotext\txt\%%~ni.txt %%i
-
-# convert PDF text into flat txt file
-#pdf2txt.py -o testout.txt ArgRregulon.pdf
-
-# REGEX for genes ^SAUSA300_[0-9][0-9][0-9][0-9]$
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # PDF_scraper
 Simple PDF scraper to select gene names generated from "RegPrecise" a
 collection of manually curated inferences of Regulons in prokaryotic Genomes
+(http://regprecise.lbl.gov/RegPrecise/)
+
+This short script is adapted to select "SAUSA300_XXXX" genes
+
+Add PDF files in the same directory and run PDF_scrapper.py 
